2be3fce6cbe88e7ee74794a275af25e1372ac48b
Andrei Pliukhaev <pluhmylkin@me.com>
2019-05-13 18:18:02 +0700

    added babel

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
-const Koa = require('koa');
+import Koa from 'koa';
 const app = new Koa();
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
+
+app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.statusCode || err.status || 500;
+      ctx.body = {
+        message: err.message
+      };
+    }
+  })
 
 app.listen(3000);
