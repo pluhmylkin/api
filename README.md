cc3be7460f594a5883ff6b0f9a31574e2e1c387d
Andrei Pliukhaev <pluhmylkin@me.com>
2019-05-14 14:04:29 +0700

    Added tslint and prettier

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # api
+
 Payments service with JSON API. API methods require authentication by passing a bearer token (https://tools.ietf.org/html/rfc6750).
 
 # Tools
 
 ## Back-end
 
-* KOA 2
-
-
+- KOA 2
 
 ## For development
 
-* Nodemon
+- Nodemon
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,20 @@
 import * as Koa from 'koa';
 const app = new Koa();
 
-app.use(async (ctx: { body: string; }) => {
+app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.use(async (ctx: { status: any; body: { message: any; }; }, next: () => void) => {
+app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
     ctx.status = err.statusCode || err.status || 500;
     ctx.body = {
-      message: err.message
+      message: err.message,
     };
   }
-})
+});
 
 app.listen(3000);
 console.log('Server running on port 3000');
-export default app;
